37db59ae642f408339814e54206040d38f52575d
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-01-17 15:03:51 +0100

    Aggiunta classe Sim

---
+++ src/com/github/redfox197/smartphone/Sim.java
@@ -0,0 +1,34 @@
+package com.github.redfox197.smartphone;
+
+public class Sim {
+    private String numero;
+    private String operatore;
+    private float credito;
+    private float costoPerMinuto;
+    private float costoSMS;
+
+    public Sim(String numero, String operatore, float credito) {
+        this.numero = numero;
+        this.operatore = operatore;
+        this.credito = credito;
+    }
+
+    public float verificaCredito() {
+        return credito;
+    }
+
+    public void ricarica(float credito) {
+        if (credito <= 0) throw new IllegalArgumentException("Credito " + credito + " non valido");
+        credito += credito;
+    }
+
+    public boolean scalaCredito(float costo) {
+        if (costo < 0) throw new IllegalArgumentException("Costo " + costo + " non valido");
+        if (credito >= costo) {
+            credito -= costo;
+            return true;
+        }
+
+        return false;
+    }
+}
